1356e36e8caef13fc204277d21b80623d7fa575c
Donald Burr <dburr@borg-cube.com>
2022-05-04 11:27:27 -0700

    Fix sync script

--- tools/sync.sh
+++ tools/sync.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # -a
-exec rsync -vh ./ dburr@borg-cube.com:/var/www/calendar.llsif.net/public_html --no-perms -O --exclude '.git' --exclude 'tools' --delete --recursive --dirs
+#exec rsync -vPh ./ dburr@borg-cube.com:/var/www/calendar.llsif.net/public_html/ --no-perms --exclude '.git' --exclude 'tools' --delete --recursive --dirs
+exec rsync -avPh ./ dburr@borg-cube.com:/var/www/calendar.llsif.net/public_html/ --no-perms --exclude '.git' --exclude 'tools' --delete  2>/dev/null
