54c0a4032b012cf0d3074ebdce39b9f7528d43b7
Donald Burr <dburr@borg-cube.com>
2022-05-04 11:31:02 -0700

    Improve readability

--- tools/sync.sh
+++ tools/sync.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # -a
 #exec rsync -vPh ./ dburr@borg-cube.com:/var/www/calendar.llsif.net/public_html/ --no-perms --exclude '.git' --exclude 'tools' --delete --recursive --dirs
-exec rsync -avPh ./ dburr@borg-cube.com:/var/www/calendar.llsif.net/public_html/ --no-perms --exclude '.git' --exclude 'tools' --delete  2>/dev/null
+exec rsync \
+  -avh --delete --no-perms \
+  --exclude '.git' \
+  --exclude 'tools' \
+  ./ dburr@borg-cube.com:/var/www/calendar.llsif.net/public_html/ 2>/dev/null
